353068834e30422052023915d24ad72fc28989af
jonhlop <jonathan.lopher@hotmail.com>
2020-05-24 22:49:31 +0200

    agregada fecha actual a los post del blog

--- src/app/new/new.component.ts
+++ src/app/new/new.component.ts
@@ -10,20 +10,24 @@ import { Router } from '@angular/router';
 })
 export class NewComponent implements OnInit {
   form: FormGroup;
-  constructor(private gestorService: GestorService, private router: Router) {}
-
-  ngOnInit(): void {
+  fechaActual: any;
+  constructor(private gestorService: GestorService, private router: Router) {
+    this.fechaActual = new Date(Date.now())
+      .toLocaleDateString()
+      .replace(/\//g, '-');
     this.form = new FormGroup({
       titulo: new FormControl('', [Validators.required]),
       autor: new FormControl('', [Validators.required]),
       categoria: new FormControl('', [Validators.required]),
-      fecha: new FormControl('', [Validators.required]),
+      fecha: new FormControl(''),
       texto: new FormControl('', [Validators.required]),
     });
   }
 
+  ngOnInit(): void {}
+
   onSubmit(post) {
-    
+    post.fecha = new Date(Date.now()).toLocaleDateString().replace(/\//g, '-');
     this.gestorService.agregarPost(post);
     this.router.navigate(['/blog']);
   }
